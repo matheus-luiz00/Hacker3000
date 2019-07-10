b1ddc0bcbfe03379a777d74a96a74c646113e6b5
Jalin Habei <Jalin.Habei@gmail.com>
2019-07-10 10:56:36 -0300

    arrays de livro

---
+++ ArrayDeDois/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDeDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeLivros = new string[2, 2]
+                {{"O pequeno Príncipe", "sim" },{"Sherlock Homes","não"} };
+            
+            for (int i = 0; i < 2; i++)
+            {
+                var nomeLivro = listaDeLivros[i,0];
+                var disponivel = listaDeLivros[i,1];
+
+                Console.WriteLine($"Nome do livro: {nomeLivro} Disponível: {disponivel}");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
