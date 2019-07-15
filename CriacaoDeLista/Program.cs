7afe9c59de331ba07b2b0b8b4c01295ef88cd23a
Matheus Luiz <hbsis@BLUE18>
2019-07-15 08:41:47 -0300

    Brincadeira de criança, como é bom!

---
+++ CriacaoDeLista/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriacaoDeLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lista = new string[10]; //Cria um array de tipo string
+
+            for (int i = 0; i < lista.Length; i++) //Preenche o array
+            {
+                lista[i] = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff");
+            }
+
+            //Escreve os dados do array no console
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
