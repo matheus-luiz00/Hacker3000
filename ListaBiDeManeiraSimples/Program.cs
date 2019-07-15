ef30a11deb53b82908db6313e62f0ab95f20458a
Matheus Luiz <hbsis@BLUE18>
2019-07-15 10:03:03 -0300

    Introdução do metodo de pesquisa em array

--- ListaBiDeManeiraSimples/Program.cs
+++ ListaBiDeManeiraSimples/Program.cs
@@ -12,19 +12,46 @@ namespace ListaBiDeManeiraSimples
         {
             string[,] listaDeNomes = new string[5, 2];
 
-            for (int i = 0; i < listaDeNomes.GetLength(0); i++)
-            {
-                listaDeNomes[i, 0] = i.ToString();
-                listaDeNomes[i, 1] = $"Salve_Guri_{i}";
-            }
+            CarregarInformacoesEListarElasEmTelas(listaDeNomes);
 
-            for (int i = 0; i < listaDeNomes.GetLength(0); i++)
-            {
-                Console.WriteLine($"ID: {listaDeNomes[i, 0]} | Nome: {listaDeNomes[i, 1]}");
-                
-            }
+
+            Console.WriteLine("Informe o ID do registro a ser pesquisado:");
+            PesquisandoInformacoesNaNossaLista( listaDeNomes, Console.ReadLine());
 
             Console.ReadLine();
+
+        }
+
+        public static void CarregarInformacoesEListarElasEmTelas( string[,] arrayBi)
+        {
+            
+
+            for (int i = 0; i < arrayBi.GetLength(0); i++)
+            {
+                arrayBi[i, 0] = i.ToString();
+                arrayBi[i, 1] = $"Salve_Guri_{i}";
+            }
+
+            for (int i = 0; i < arrayBi.GetLength(0); i++)
+            {
+                Console.WriteLine($"ID: {arrayBi[i, 0]} | Nome: {arrayBi[i, 1]}");
+
+            }
+        }
+
+
+        public static void PesquisandoInformacoesNaNossaLista( string[,] arrayBi, string pId)
+        {
+            for (int i = 0; i < arrayBi.GetLength(0); i++)
+            {
+                if (arrayBi[i,0] == pId)
+                {
+                    Console.WriteLine($"Informação escolhida: ID {arrayBi[i, 0]} | Nome {arrayBi[i, 1]}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Infelizmente a busca pelo id não resultou em nenhuma informação");
         }
     }
 }
