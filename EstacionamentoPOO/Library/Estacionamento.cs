35768eedd5f4c6b3cb8abc56452ff84ce4007bf4
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-25 17:26:06 -0300

    Estacionamento

---
+++ EstacionamentoPOO/Library/Estacionamento.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoPOO.Library
+{
+    public class Estacionamento
+    {
+        string[,] registros = new string[5, 5];
+
+        public void Entrada(string modelo, string placa)
+        {
+            for (int i = 0; i < registros.GetLength(0); i++)
+            {
+                if (registros[i, 1] != "-1")
+                {
+                    registros[i, 0] = (i + 1).ToString(); 
+                    registros[i, 1] = modelo;
+                    registros[i, 2] = placa;
+                    registros[i, 3] = DateTime.Now.ToString("dd/MM/yyyy");
+                    registros[i, 4] = "-1";
+                    break;
+                }
+            }
+        }
+
+        public void Entrada(int id)
+        {
+            for (int i = 0; i < registros.GetLength(0); i++)
+            {
+                if (registros[i, 0] == id.ToString())
+                {
+                    registros[i, 3] = DateTime.Now.ToString("dd/MM/yyyy");
+                    registros[i, 4] = "-1";
+                    break;
+                }
+            }
+        }
+
+        public void Saida(int id)
+        {
+            for (int i = 0; i < registros.GetLength(0); i++)
+            {
+                if (id.ToString() != registros[i, 0]) continue;
+
+                registros[i, 4] = DateTime.Now.ToString("dd/MM/yyyy");
+
+            }
+        }
+
+        public void Listar()
+        {
+            for (int i = 0; i < registros.GetLength(0); i++)
+            {
+                Console.WriteLine(  string.Format(" {0,-3} | {1,-20} | {2,8} | {3,10} | {4}",
+                    registros[i, 0], registros[i, 1], registros[i, 2], registros[i, 3], (registros[i, 4] != "-1")? registros[i, 4]:"NULO"));
+            }
+        }
+
+        public void ListarSemSaida()
+        {
+
+            for (int i = 0; i < registros.GetLength(0); i++)
+            {
+                if (registros[i, 4] != "-1") continue;
+                Console.WriteLine(  string.Format(" {0,-3} | {1,-20} | {2,8} | {3,10} | {4}",
+                    registros[i, 0], registros[i, 1], registros[i, 2], registros[i, 3], (registros[i, 4] != "-1") ? registros[i, 4] : "NULO"));
+            }
+        }
+
+        public void Listar(string pesq)
+        {
+            for (int i = 0; i < registros.GetLength(0); i++)
+            {
+                if (registros[i, 2] != pesq) continue;
+                Console.WriteLine(string.Format(" {0,-3} | {1,-20} | {2,8} | {3,10} | {4}",
+                    registros[i, 0], registros[i, 1], registros[i, 2], registros[i, 3], (registros[i, 4] != "-1") ? registros[i, 4] : "NULO"));
+                return;
+            }
+            Console.WriteLine("Placa não encontrada");
+        }
+    }
+}
---
+++ EstacionamentoPOO/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstacionamentoPOO.Library;
+
+namespace EstacionamentoPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estacionamento carros = new Estacionamento();
+            int escolha = -1;
+
+
+            while(escolha != 0)
+            {
+                escolha = menuPrincipal();
+                switch (escolha)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            EntradaSimples(carros);
+                            
+                            break;
+                        };
+                    case 2:
+                        {
+                            Console.Clear();
+                            EntradaID(carros);
+                            break;
+                        };
+                    case 3:
+                        {
+                            Console.Clear();
+                            carros.Listar();
+                            Console.WriteLine("Pressione ENTER para voltar ao menu principal");
+                            Console.ReadLine();
+                            break;
+                        };
+                    case 4:
+                        {
+                            Console.Clear();
+                            carros.ListarSemSaida();
+                            Console.WriteLine("Pressione ENTER para voltar ao menu principal");
+                            Console.ReadLine();
+                            break;
+                        };
+                    case 5:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Digite a placa do veículo a ser pesquisado");
+                            carros.Listar(Console.ReadLine());
+                            Console.WriteLine("Pressione ENTER para voltar ao menu principal");
+                            Console.ReadLine();
+                            break;
+                        };
+                    case 6:
+                        {
+                            Console.Clear();
+                            carros.ListarSemSaida();
+                            Console.WriteLine("Digite ID do veículo para dar baixa");
+                            int.TryParse(Console.ReadLine(), out int id);
+                            carros.Saida(id);
+                            Console.WriteLine("Operação finalizada");
+                            Console.WriteLine("Pressione ENTER para voltar ao menu principal");
+                            Console.ReadLine();
+                            break;
+                        };
+
+
+                    case 0:
+                        {
+                            escolha = 0;
+                            break;
+                        };
+                }
+            }
+        }
+
+        static void EntradaSimples(Estacionamento cr)
+        {
+            Console.WriteLine("Digite o modelo do veículo:");
+            var mod = Console.ReadLine();;
+            Console.WriteLine("Digite a placa do veículo:");
+            var placa = Console.ReadLine();;
+            cr.Entrada(mod, placa);
+            Console.WriteLine("Operação realizada com sucesso\n");
+            Console.WriteLine("Pressione ENTER para voltar ao menu principal");
+            Console.ReadLine();
+        }
+
+        static void EntradaID(Estacionamento cr)
+        {
+            Console.WriteLine("Digite o ID do veículo:");
+            int.TryParse(Console.ReadLine(), out int id);
+            cr.Entrada(id);
+            Console.WriteLine("Operação realizada com sucesso\n");
+            Console.WriteLine("Pressione ENTER para voltar ao menu principal");
+            Console.ReadLine();
+        }
+        static int menuPrincipal()
+        {
+            Console.Clear();
+
+            mostrarSejaBemVindo();
+
+            Console.WriteLine("\nMenu Inicial");
+            separador(30);
+            Console.WriteLine("O que você deseja realizar?\n");
+            Console.WriteLine("1 - Entrada em um veículo");
+            Console.WriteLine("2 - Reentrada em um veículo");
+            Console.WriteLine("3 - Listar TODOS os veículos");
+            Console.WriteLine("4 - Listar veículos SEM SAIDA");
+            Console.WriteLine("5 - Pesquisar veículo por placa");
+            Console.WriteLine("6 - Dar saída em um veículo");
+            Console.WriteLine("0 - Sair do sistema");
+            separador(30);
+            Console.WriteLine("Digite o número desejado:");
+            int.TryParse(Console.ReadLine(), out int opcao);
+
+            return opcao;
+        }
+
+        static void mostrarSejaBemVindo()
+        {
+            separador(60);
+            Console.WriteLine("            SISTEMA ESTACIONAMENTO");
+            separador(60);
+        }
+
+        static void separador(int n)
+        {
+            string line = new string('-', n);
+            Console.WriteLine(line);
+        }
+    }
+}
