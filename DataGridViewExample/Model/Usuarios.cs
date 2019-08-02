851e9d705c85852787b54ecb3e7244a40914715d
Treinamento HBSIS <proway@hbsis.com.br>
2019-08-02 10:47:04 -0300

    Tirado a definição padrão de usuinc usualt das classes

--- DataGridViewExample/Model/Usuarios.cs
+++ DataGridViewExample/Model/Usuarios.cs
@@ -10,8 +10,8 @@ namespace DataGridViewExample.Model
     {
         public string Usuario { get; set; }
         public bool Ativo { get; set; } = true;
-        public int UsuInc { get; set; } = 1;
-        public int UsuAlt { get; set; } = 1;
+        public int UsuInc { get; set; } 
+        public int UsuAlt { get; set; }
         public DateTime DatInc { get; set; } = DateTime.Now;
         public DateTime DatAlt { get; set; } = DateTime.Now;
     }
