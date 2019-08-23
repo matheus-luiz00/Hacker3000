a199b53f00b55351a84e5e861d3170f42ddc601e
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-23 17:16:23 -0300

    .

---
+++ RevisaoWEBApi/Models/CustomValidFields.cs
@@ -0,0 +1,98 @@
+﻿using RevisaoWEBApi.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace RevisaoWEBApi.Models
+{
+    public class CustomValidFields : ValidationAttribute
+    {
+        ContextDB db = new ContextDB();
+
+        private ValidFields typeField;
+
+        public CustomValidFields(ValidFields  type)
+        {
+            typeField = type;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+
+                switch (typeField)
+                {
+                    case ValidFields.ValidaLogin:
+                        {
+                            return ValidarLogin(value);
+                        }
+                    case ValidFields.ValidaEmail:
+                        {
+                            return ValidarEmail(value, validationContext.DisplayName);
+                        }
+                    case ValidFields.ValidaSenha:
+                        {
+                            return ValidarSenha(value);
+                        }
+                    case ValidFields.ValidaNome:
+                        {
+                            return ValidarNome(value);
+                        }
+                }
+
+            }
+
+            return new ValidationResult($"O campo {validationContext.DisplayName} é obrigatorio");
+        }
+
+        private ValidationResult ValidarEmail(object value, string displayField)
+        {
+            var result = Regex.IsMatch(value.ToString(), @"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
+
+            if (result)
+                return ValidationResult.Success;
+
+            return new ValidationResult($"O campo {displayField} é invalido");
+        }
+
+        private ValidationResult ValidarLogin(object value)
+        {
+            Usuario user = db.usuarios.FirstOrDefault(x => x.Login == value.ToString());
+
+            if (user == null)
+                return ValidationResult.Success;
+            
+            return new ValidationResult($"O usuario já existe");
+
+        }
+
+        private ValidationResult ValidarNome(object value)
+        {
+            
+            var result = Regex.IsMatch(value.ToString(), @"[0-9]");
+
+            if (!result)
+                return ValidationResult.Success;
+
+            return new ValidationResult($"Nome inválido");
+        }
+
+        private ValidationResult ValidarSenha(object value)
+        {
+            if (value.ToString().Length < 8)
+                return new ValidationResult("A senha é menor que o mínimo de 8 caracteres");
+            else if (value.ToString().Length > 16)
+                return new ValidationResult("A senha é maior que o máximo de 16 caracteres");
+
+            return ValidationResult.Success;
+        }
+
+    }
+
+
+}
---
+++ RevisaoWEBApi/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RevisaoWEBApi.Models
+{
+    public class Usuario : UserControls
+    {
+        public int Id { get; set; }
+
+        [CustomValidFields(Enums.ValidFields.ValidaNome)]
+        public string Nome { get; set; }
+
+        [CustomValidFields(Enums.ValidFields.ValidaEmail)]
+        public string Email { get; set; }
+
+        [CustomValidFields(Enums.ValidFields.ValidaLogin)]
+        public string Login { get; set; }
+        [MaxLength(16, ErrorMessage = "O campo excedeu o limite de 16 caracteres")]
+        [MinLength(8, ErrorMessage = "O campo não atingiu um mínimo de 8 caracteres")]
+
+        [CustomValidFields(Enums.ValidFields.ValidaSenha)]
+        public string Senha { get; set; }
+    }
+}
